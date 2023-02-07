64848bdc9302647e03cb622a1398bce8b513fd1d
gaurav20619 <gauravsinghraijada619@gmail.com>
2023-02-07 09:10:23 +0530

    Add: program to let user choose a letter between O or X.

--- src/TicTacToe.java
+++ src/TicTacToe.java
@@ -1,7 +1,14 @@
+import java.util.Scanner;
+
 public class TicTacToe {
     static char [] board = new char[10];
+    static char userLetter;
+    static char computerLetter;
+
     public static void main(String[] args) {
         createEmptyBoard();
+        chooseLetter();
+
     }
 
     // UC1- TO create an empty board of size 10.
@@ -12,5 +19,20 @@ public class TicTacToe {
             board[index]= ' ';
         }
     }
+    // UC2- to choose a letter for user and computer
+    public static void chooseLetter()
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("choose a letter: X or O");
+        userLetter = sc.next().toUpperCase().charAt(0);
+        if (userLetter=='X') {
+            computerLetter = 'O';
+
+        } else {
+            computerLetter = 'X';
+        }
+    }
+
+    
 
 }
