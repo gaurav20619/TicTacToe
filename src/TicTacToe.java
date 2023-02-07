747599913f4aa43332ef719ffac2f78b94847132
gaurav20619 <gauravsinghraijada619@gmail.com>
2023-02-07 09:26:40 +0530

    Add: program to show the tic tac toe.

--- src/TicTacToe.java
+++ src/TicTacToe.java
@@ -8,6 +8,7 @@ public class TicTacToe {
     public static void main(String[] args) {
         createEmptyBoard();
         chooseLetter();
+        showBoard();
 
     }
 
@@ -32,7 +33,13 @@ public class TicTacToe {
             computerLetter = 'X';
         }
     }
-
-    
+    // UC3- To display the tic tac toe. 
+    public static void showBoard() {
+        System.out.println(board[1]+ " | "+ board[2]+ " | "+ board[3]);
+        System.out.println("---------");
+        System.out.println(board[4]+ " | "+ board[5]+ " | "+ board[6]);
+        System.out.println("---------");
+        System.out.println(board[7]+ " | "+ board[8]+ " | "+ board[9]);
+        }
 
 }
